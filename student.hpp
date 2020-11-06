9837aea6753ebe6014c87966ffa15112e6be15d9
bernardoS401 <70169558+bernardoS401@users.noreply.github.com>
2020-11-06 16:53:16 -0300

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -3,7 +3,7 @@
 #include <iostream> // print library
 
 // Exercise 1: implements function 'add', that adds two numbers
-int add(int a, int b);
+int add(int a=1, int b=2);
 
 // Exercise 2: implements function 'mul', that multiplies two numbers
 int mul(int a, int b);
